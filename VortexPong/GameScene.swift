895671f3b67eb2420ab69a867899b85253713598
Joseph Hansen <theythemtheir@icloud.com>
2017-01-10 14:17:46 -0700

    initial commit
    initial commit,
    very simple version of game

---
+++ VortexPong/GameScene.swift
@@ -0,0 +1,108 @@
+//
+//  GameScene.swift
+//  VortexPong
+//
+//  Created by Joseph Hansen on 12/29/16.
+//  Copyright © 2017 Joseph Hansen. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+   
+    var ball = SKSpriteNode()
+    var enemy = SKSpriteNode()
+    var main = SKSpriteNode()
+    
+    var topLbl = SKLabelNode()
+    var btmLbl = SKLabelNode()
+    
+    var score = [Int]()
+    
+    override func didMove(to view: SKView) {
+        
+        startGame()
+        
+        topLbl = self.childNode(withName: "topLabel") as! SKLabelNode
+        btmLbl = self.childNode(withName: "btmLabel") as! SKLabelNode
+        
+        
+        ball = self.childNode(withName: "ball") as! SKSpriteNode
+        enemy = self.childNode(withName: "enemy") as! SKSpriteNode
+        main = self.childNode(withName: "main") as! SKSpriteNode
+        
+        ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
+        
+        let border  = SKPhysicsBody(edgeLoopFrom: self.frame)
+        
+        border.friction = 0
+        border.restitution = 1
+        
+        self.physicsBody = border
+    }
+    
+    func startGame() {
+        score = [0,0]
+        topLbl.text = "\(score[1])"
+        btmLbl.text = "\(score[0])"
+    }
+
+    func addScore(playerWhoWon : SKSpriteNode){
+        
+        ball.position = CGPoint(x: 0, y: 0)
+        ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        
+        if playerWhoWon == main {
+            score[0] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
+        }
+        else if playerWhoWon == enemy {
+            score[1] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: -20, dy: -20))
+        }
+        
+        topLbl.text = "\(score[1])"
+        btmLbl.text = "\(score[0])"
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        for touch in touches {
+            let location = touch.location(in: self)
+            
+            main.run(SKAction.moveTo(x: location.x, duration: 0.2))
+            
+        }
+        
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            
+            
+            main.run(SKAction.moveTo(x: location.x, duration: 0.2))
+            
+        }
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+        
+        enemy.run(SKAction.moveTo(x: ball.position.x, duration: 1.0))
+        
+        if ball.position.y <= main.position.y - 70 {
+            addScore(playerWhoWon: enemy)
+        }
+        else if ball.position.y >= enemy.position.y + 70 {
+            addScore(playerWhoWon: main)
+        }
+    }
+}
+
+
+
+
+
+
+
